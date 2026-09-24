f0a29c693af9bf8c0fd3efd7bf36420f5887e48c
Simon <s91k@hotmail.com>
2026-09-24 17:35:29 +0200

    add unit tests for BookController and BookService

--- backend/src/main/java/s91k/github/io/bookstore_api/entity/Book.java
+++ backend/src/main/java/s91k/github/io/bookstore_api/entity/Book.java
@@ -1,10 +1,12 @@
 package s91k.github.io.bookstore_api.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
